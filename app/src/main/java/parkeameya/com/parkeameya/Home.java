51497c152815cc33a3cee675c8ca477148c2d4b4
johnalexquispelucas <johnalexql@gmail.com>
2017-11-07 00:55:05 -0500

    parkeameya v1.1

--- app/src/main/java/parkeameya/com/parkeameya/Home.java
+++ app/src/main/java/parkeameya/com/parkeameya/Home.java
@@ -1,8 +1,17 @@
 package parkeameya.com.parkeameya;
 
+import android.*;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,19 +21,59 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.firebase.geofire.GeoFire;
+import com.firebase.geofire.GeoLocation;
+import com.github.glomadrian.materialanimatedswitch.MaterialAnimatedSwitch;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Home extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener,OnMapReadyCallback {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     SupportMapFragment mapFragment;
+
+    private GoogleMap mMap;
+    private GoogleMap Mymap;
+    //play services
+
+    private static final int MY_PERMISSION_REQUEST_CODE = 7192;
+    private static final int PLAY_SERVICE_RES_REQUEST = 300193;
+
+    private LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLastLocation;
+
+    private static int UPDATE_INTERVAL = 5000;  // 5 seg
+    private static int FATEST_INTERVAL = 3000;
+    private static int DISPLACEMENT = 10;
+
+    DatabaseReference ref;
+    GeoFire geoFire;
+    Marker mUserMarker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +82,6 @@ public class Home extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setNavigationIcon(R.drawable.logo);
-
 
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -48,6 +96,126 @@ public class Home extends AppCompatActivity
         //Maps
         mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        //Geo Fire
+        ref = FirebaseDatabase.getInstance().getReference("Drivers");
+        geoFire = new GeoFire(ref);
+
+        setUpLocation();
+    }
+
+    //presionar ctrl + 0
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (checkPlayServices()) {
+                        buildGoogleApiClient();
+                        createLocationRequest();
+
+                        displayLocation();
+                    }
+                }
+                break;
+        }
+    }
+
+    private void setUpLocation() {
+
+        //copiar el mismo codigo de driver app :v
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //Request runtime permission
+            ActivityCompat.requestPermissions(this, new String[]{
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+            }, MY_PERMISSION_REQUEST_CODE);
+        } else {
+            if (checkPlayServices()) {
+                buildGoogleApiClient();
+                createLocationRequest();
+                displayLocation();
+            }
+        }
+
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+    }
+
+    private void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+
+
+    }
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode))
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this, PLAY_SERVICE_RES_REQUEST).show();
+            else {
+                Toast.makeText(this, "este dispositivo no es compatible", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+
+    private void displayLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (mLastLocation != null) {
+
+            final double latitude = mLastLocation.getLatitude();
+            final double longitude = mLastLocation.getLongitude();
+
+            //Update to Firebase
+            geoFire.setLocation(FirebaseAuth.getInstance().getCurrentUser().getUid(), new GeoLocation(latitude, longitude), new GeoFire.CompletionListener() {
+                @Override
+                public void onComplete(String key, DatabaseError error) {
+                    //Add Marker
+                    if (mUserMarker != null)
+                        mUserMarker.remove(); //remove already marker
+                    mUserMarker = mMap.addMarker(new MarkerOptions()
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.car))
+                            .position(new LatLng(latitude, longitude))
+                            .title(""));
+                   //         .title(String.format("tu ubicacion")));
+
+                    //Move camera to this position
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude, longitude), 15.0f));
+
+                    //Draw animation rotate marker
+                    //         rotateMarker(mCurrent,-360,mMap);
+                }
+            });
+
+            Log.d("EDMTDEV", String.format("your location vas changed : %f / %f", latitude, longitude));
+        } else {
+            Log.d("EDMTDEV", "Cannot get your location");
+        }
+
+
     }
 
     @Override
@@ -109,12 +277,86 @@ public class Home extends AppCompatActivity
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
-        //add Sample marker
-        googleMap.addMarker(new MarkerOptions()
-                    .position(new LatLng(37.750,-122.4183))
-                    .title("San Francisco"));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.750,-122.4183),12));
+        John(googleMap);
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setTrafficEnabled(false);
+        mMap.setIndoorEnabled(false);
+        mMap.setBuildingsEnabled(false);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
 
     }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        displayLocation();
+        startLocationUpdates();
+
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        displayLocation();
+
+    }
+    public void John(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+
+        final LatLng punto1 = new LatLng(-12.0580334, -77.0793496);
+        final LatLng punto2 = new LatLng(-12.056377, -77.079175);
+        final LatLng punto3 = new LatLng(-12.059199, -77.078070);
+        final LatLng punto4 = new LatLng(-12.060678, -77.080420);
+        final LatLng punto5 = new LatLng(-12.058255, -77.078971);
+        final LatLng punto6 = new LatLng(-12.055212, -77.080720);
+        final LatLng punto7 = new LatLng(-12.055139, -77.079476);
+        final LatLng punto8 = new LatLng(-12.055076, -77.081836);
+        mMap.addMarker(new MarkerOptions().position(punto1).title("Cochera1"));
+        mMap.addMarker(new MarkerOptions().position(punto2).title("Cochera2"));
+        mMap.addMarker(new MarkerOptions().position(punto3).title("Cochera3"));
+        mMap.addMarker(new MarkerOptions().position(punto4).title("Cochera4"));
+        mMap.addMarker(new MarkerOptions().position(punto5).title("Cochera5"));
+        mMap.addMarker(new MarkerOptions().position(punto6).title("Cochera6"));
+        mMap.addMarker(new MarkerOptions().position(punto7).title("Cochera7"));
+        mMap.addMarker(new MarkerOptions().position(punto8).title("Cochera8"));
+
+
+        //me manda  a otra interfaz
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent intent = new Intent(Home.this,Parkeo.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+
+    }
+
 }
+
---
+++ app/src/main/java/parkeameya/com/parkeameya/Parkeo.java
@@ -0,0 +1,13 @@
+package parkeameya.com.parkeameya;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Parkeo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parkeo);
+    }
+}
